b2a37a4859ef670cd722ca615596fe9f08d34b9c
paulo-valleriote <paulomv29@outlook.com>
2025-06-20 11:29:03 -0300

    feat: api core initial setup

---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"healthclinic/config"
+	"log"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	env := config.InitEnvVariables()
+	db := config.InitDB()
+
+	diContainer := config.NewDIContainer(
+		e,
+		db,
+		env,
+	)
+
+	err := initRoutes(*diContainer)
+	if err != nil {
+		log.Fatal("Erro ao iniciar as rotas da API")
+	}
+
+	e.Logger.Fatal(e.Start(":" + env.Port()))
+}
---
+++ cmd/routes.go
@@ -0,0 +1,8 @@
+package main
+
+import "healthclinic/config"
+
+func initRoutes(diContainer config.DIContainer) error {
+	// Criar rotas
+	return nil
+}
